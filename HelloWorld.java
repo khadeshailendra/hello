b38acaaa2a928f500c55a187400b7fd5051f2e8c
khadeshailendra <khadeshailendra@gmail.com>
2016-08-18 12:37:05 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World, from shailendra");
+        System.out.println("Hello, World, from Prashant");
     }
 
 }
